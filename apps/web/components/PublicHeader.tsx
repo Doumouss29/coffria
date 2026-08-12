7acfed598f99cb708f03d0ca45dc83ac80af2fa1
Doumouss29 <48911588+Doumouss29@users.noreply.github.com>
2026-08-12 14:02:54 +0200

    Ajout du lien Offres dans la navigation publique

--- apps/web/components/PublicHeader.tsx
+++ apps/web/components/PublicHeader.tsx
@@ -6,10 +6,11 @@ export default function PublicHeader() {
       <div className="publicContainer publicHeaderInner">
         <Link href="/" className="publicBrandWrap">
           <span className="publicBrand">Coffr<span>i</span>a</span>
-          <span className="publicBrandTagline">Une solution LMurbs</span>
+          <small>Une solution LMurbs</small>
         </Link>
         <nav className="publicNav">
           <Link href="/">Accueil</Link>
+          <Link href="/offres">Offres</Link>
           <Link href="/tarifs">Tarifs</Link>
           <Link href="/contact">Contact</Link>
           <Link href="/connexion" className="publicLoginButton">Connexion</Link>
