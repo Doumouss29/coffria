c8e2ee6cf6df0872faed91619a1d283a1c9c64ac
Doumouss29 <48911588+Doumouss29@users.noreply.github.com>
2026-08-12 13:24:33 +0200

    Ajout navigation publique Coffria

---
+++ apps/web/components/PublicHeader.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function PublicHeader() {
+  return (
+    <header className="publicHeader">
+      <div className="publicContainer publicHeaderInner">
+        <Link href="/" className="publicBrand">Coffr<span>i</span>a</Link>
+        <nav className="publicNav">
+          <Link href="/">Accueil</Link>
+          <Link href="/tarifs">Tarifs</Link>
+          <Link href="/contact">Contact</Link>
+          <Link href="/connexion" className="publicLoginButton">Connexion</Link>
+        </nav>
+      </div>
+    </header>
+  );
+}
