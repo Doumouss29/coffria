f5bc0f3c0518f27eb25d77578f0dbeb2edf5d4cf
Doumouss29 <48911588+Doumouss29@users.noreply.github.com>
2026-08-12 13:45:57 +0200

    Ajout mention Une solution LMurbs

--- apps/web/components/PublicHeader.tsx
+++ apps/web/components/PublicHeader.tsx
@@ -4,7 +4,10 @@ export default function PublicHeader() {
   return (
     <header className="publicHeader">
       <div className="publicContainer publicHeaderInner">
-        <Link href="/" className="publicBrand">Coffr<span>i</span>a</Link>
+        <Link href="/" className="publicBrandWrap">
+          <span className="publicBrand">Coffr<span>i</span>a</span>
+          <span className="publicBrandTagline">Une solution LMurbs</span>
+        </Link>
         <nav className="publicNav">
           <Link href="/">Accueil</Link>
           <Link href="/tarifs">Tarifs</Link>
